775d6b7316941c798b8db292a7cb2f970ed7f75e
Rory Wirch <rwirch@redhat.com>
2021-07-07 11:20:03 -0400

    outputs proper yaml for every cloc'd hash

--- ceph_cloc.sh
+++ ceph_cloc.sh
@@ -1,15 +1,31 @@
 #!/bin/sh
+BASE_DIR=$(pwd)
 FILE="test_hashes.txt"
+TEMP_YAML="temp_yaml.yaml"
+CLOC_REPORT="cloc_report.yaml"
 
 #Uncomment after testing
 #should also check that shell scripts are executable
 #sh ./setup.sh
 
 #Read hashes from file, run cloc with each hash in ceph repo
-while IFS= read -r line
+while IFS= read -r hash
 do
-    echo "$line"
+    echo "$hash"
     cd ceph
-    cloc . --git $line
+    cloc . --git $hash --yaml --out=$TEMP_YAML
+
+    printf "hash:\"${hash}\"\n" >> $BASE_DIR/$CLOC_REPORT
+
+    while IFS= read -r line
+    do
+        if [ "$line" != "---" ]
+        then
+            printf "  ${line}\n" >> $BASE_DIR/$CLOC_REPORT
+        fi
+    done < $TEMP_YAML
+
+    rm $TEMP_YAML
     cd ..
+    
 done < $FILE
