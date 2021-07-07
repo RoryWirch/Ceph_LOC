f4c0c0a50b18fbdc6e42c1ffc791266d841d6bdd
Rory Wirch <rwirch@redhat.com>
2021-07-07 11:52:55 -0400

    moved code to cloc_hash function

--- ceph_cloc.sh
+++ ceph_cloc.sh
@@ -8,7 +8,27 @@ CLOC_REPORT="cloc_report.yaml"
 #should also check that shell scripts are executable
 #sh ./setup.sh
 
+# Runs cloc on a given hash, formats yaml output and adds to cloc report
+cloc_hash()
+{
+    hash=$1
 
+    cd ceph
+    cloc . --git $hash --yaml --out=$TEMP_YAML
+
+    printf "hash:\"${hash}\"\n" >> $BASE_DIR/$CLOC_REPORT
+
+    while IFS= read -r line
+    do
+        if [ "$line" != "---" ]
+        then
+            printf "  ${line}\n" >> $BASE_DIR/$CLOC_REPORT
+        fi
+    done < $TEMP_YAML
+
+    rm $TEMP_YAML
+    cd ..
+}
 
 #Read hashes from file, run cloc on each hash in ceph repo
 while IFS= read -r hash
@@ -17,21 +37,6 @@ do
     then
         echo "${hash} has already been cloc'd"
     else
-        cd ceph
-        cloc . --git $hash --yaml --out=$TEMP_YAML
-
-        printf "hash:\"${hash}\"\n" >> $BASE_DIR/$CLOC_REPORT
-
-        while IFS= read -r line
-        do
-            if [ "$line" != "---" ]
-            then
-                printf "  ${line}\n" >> $BASE_DIR/$CLOC_REPORT
-            fi
-        done < $TEMP_YAML
-
-        rm $TEMP_YAML
-        cd ..
+        cloc_hash $hash
     fi
-    
 done < $FILE
