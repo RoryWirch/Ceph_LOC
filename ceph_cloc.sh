b27a9b368410a969a490dce8ace2a83a3e801673
Rory Wirch <rwirch@redhat.com>
2021-07-07 09:24:07 -0400

    runs cloc on test hashes

--- ceph_cloc.sh
+++ ceph_cloc.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
-FILE="hashes.txt"
+FILE="test_hashes.txt"
 
 #Uncomment after testing
 #should also check that shell scripts are executable
 #sh ./setup.sh
 
-#Read hashes from file
+#Read hashes from file, run cloc with each hash in ceph repo
 while IFS= read -r line
 do
     echo "$line"
+    cd ceph
+    cloc . --git $line
+    cd ..
 done < $FILE
